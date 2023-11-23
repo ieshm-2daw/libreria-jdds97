7a18b77002f6b91464291e013479a7461ab24daf
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-11-23 12:45:13 +0100

    Primer commit

---
+++ biblioteca/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator 
+
+class Usuario(AbstractUser):
+    dni=models.CharField(max_length=9)
+    direccion=models.TextField()
+    telefono=models.IntegerField(validators=[MaxValueValidator(9)])
+class Libro(models.Model):
+    título=models.TextField()
