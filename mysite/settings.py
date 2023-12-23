222a006502d100eb03587672120d8ce34c446463
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-12-23 15:25:56 +0100

    wsgi configuracion

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ AUTH_USER_MODEL = "biblioteca.Usuario"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.100.5"]
 
 
 # Application definition
